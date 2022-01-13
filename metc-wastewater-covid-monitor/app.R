749ad1c9eac3984adce2601418195314c06914c9
Ashley Asmus <ashley.asmus@gmail.com>
2022-01-13 11:27:18 -0600

    adjust padding on load fig

--- metc-wastewater-covid-monitor/app.R
+++ metc-wastewater-covid-monitor/app.R
@@ -74,33 +74,55 @@ server <- function(input, output) {
             tickfont = list(color = colors$suppGray),
             overlaying = "y",
             side = "right",
-            title = list(text = "<b>Reported COVID-19 cases,</b> 7-day average", standoff = 25L),
-            zerolinewidth = 2, zerolinecolor = '#ffff',
-            gridcolor = 'ffff')
+            title = list(text = "<b>Reported COVID-19 cases,</b> 7-day average", standoff = 25),
+            zerolinewidth = 2,
+            zerolinecolor = '#ffff',
+            gridcolor = 'ffff'
+        )
         
-
         
-        load_plot <- 
+        
+        load_plot <-
             load_data %>%
             left_join(case_data) %>%
-            plot_ly(type = 'scatter', mode = 'lines')%>%
-            add_trace(x = ~date, y = ~copies_day_person_M_mn, name = 'Viral load',
-                      size = 1, 
-                      yaxis = "y2", 
-                      fill = 'tozeroy',
-                      fillcolor = 'rgba(0, 154, 199, .5)', 
-                      line = list(width = 0.5, color = colors$esBlue))%>%
-            add_trace(x = ~date, y = ~covid_cases_7day, 
-                      name = "Cases per 100,000",
-                      fill = 'tozeroy',
-                      fillcolor = 'rgba(140, 140, 140, .8)', 
-                      line = list(width = 0.5, color = "gray50")) %>%
+            plot_ly(type = 'scatter', mode = 'lines') %>%
+            add_trace(
+                x = ~ date,
+                y = ~ copies_day_person_M_mn,
+                name = 'Viral load',
+                size = 1,
+                yaxis = "y2",
+                fill = 'tozeroy',
+                fillcolor = 'rgba(0, 154, 199, .5)',
+                line = list(width = 0.5, color = colors$esBlue)
+            ) %>%
+            add_trace(
+                x = ~ date,
+                y = ~ covid_cases_7day,
+                name = "Cases per 100,000",
+                fill = 'tozeroy',
+                fillcolor = 'rgba(140, 140, 140, .3)',
+                line = list(width = 0.5, color = colors$suppGray)
+            ) %>%
             layout(showlegend = F) %>%
             layout(
+                margin = list(l = 100, r = 100, b = 100),
                 yaxis2 = ay,
-                xaxis = list(title= list(text = "Date", standoff = 25), zerolinewidth = 2, gridcolor = 'ffff', zerolinecolor = '#ffff'),
-                yaxis = list(title= list(text = "<b>Viral load</b>in wastewater", standoff = 25), zerolinewidth = 2, gridcolor = 'ffff', zerolinecolor = '#ffff')
+                xaxis = list(
+                    title = list(text = "Date", standoff = 25),
+                    zerolinewidth = 2,
+                    gridcolor = 'ffff',
+                    zerolinecolor = '#ffff'
+                ),
+                yaxis = list(
+                    title = list(text = "<b>Viral load</b>in wastewater", standoff = 25),
+                    zerolinewidth = 2,
+                    gridcolor = 'ffff',
+                    zerolinecolor = '#ffff'
+                )
             )
+        
+        load_plot
     })
     
     
