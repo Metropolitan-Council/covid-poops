378dbafb687dc2c2d8507128235e9bdda1ecf0ed
Ashley Asmus <ashley.asmus@gmail.com>
2022-01-24 09:56:08 -0600

    Create fig_sewershed.R
    getting started w/ a map of the sewershed

---
+++ R/fig_sewershed.R
@@ -0,0 +1,16 @@
+library(sf)
+library(tidyverse)
+library(councilR)
+# Open Geopackage
+
+pooshed <- councilR::import_from_gpkg('https://resources.gisdata.mn.gov/pub/gdrs/data/pub/us_mn_state_metc/util_sanitary_sewersheds/gpkg_util_sanitary_sewersheds.zip')
+  
+simple_shed < 
+pooshed %>%
+  group_by(WWTP) %>% # WWTP = wastewater treatment plant
+  summarize(geom = st_union(geom)) %>%
+  st_simplify()
+
+
+nrow(pooshed)
+
