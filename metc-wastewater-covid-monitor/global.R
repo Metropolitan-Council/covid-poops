1d40451cfc24fe353dd98d29f801681fe66a4402
Liz Roten <liz.roten@metc.state.mn.us>
2022-02-18 10:03:28 -0600

    adjust annotation position

--- metc-wastewater-covid-monitor/global.R
+++ metc-wastewater-covid-monitor/global.R
@@ -68,7 +68,7 @@ ann_list <- list(
     color = councilR::colors$suppBlack
   ),
   x = 1,
-  y = -0.15,
+  y = -0.2,
   showarrow = F,
   xref = "paper", yref = "paper",
   xanchor = "right", yanchor = "auto",
