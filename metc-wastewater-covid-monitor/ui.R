5ee77a7c74f44b8b2e1e882c80ef50befd43289a
lfletch0025 <laura.fletcher@metc.state.mn.us>
2022-06-02 09:06:05 -0500

    Data update

--- metc-wastewater-covid-monitor/ui.R
+++ metc-wastewater-covid-monitor/ui.R
@@ -40,7 +40,7 @@ fluidPage(
             "COVID-19 Prevalence",
             HTML("<h6><section style='font-size:14pt'>This graph shows the amount of SARS-CoV-2 viral RNA detected in Metro Plant wastewater influent (blue line) and the number of COVID-19 cases in the seven-county area (gray line).</h3>"
             ),
-            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 23, 2022.</h3>"
+            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 30, 2022.</h3>"
             ),
             plotlyOutput("loadPlot", height = "auto")
           ),
@@ -49,7 +49,7 @@ fluidPage(
             "Variant Prevalence",
             HTML("<h6><section style='font-size:14pt'>This graph shows the estimated amount of SARS-CoV-2 viral RNA by COVID-19 variant, sub-variant and lineage. The total amount of SARS-CoV-2 viral RNA in Metro Plant wastewater influent is shown in the background in gray.</h3>"
             ),
-            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 23, 2022.</h3>"
+            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 30, 2022.</h3>"
             ),
             plotlyOutput("variantLoadPlot", height = "auto")
           ),
@@ -58,24 +58,27 @@ fluidPage(
             "Variant Frequencies (%)",
             HTML("<h6><section style='font-size:14pt'>This graph shows the estimated percent of SARS-CoV-2 viral RNA contributed by COVID-19 variant, sub-variant and lineage.</h3>"
             ),
-            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 23, 2022.</h3>"
+            HTML("<h6><section style='font-size:12pt;font-style:italic'>Last Sample Date: May 30, 2022.</h3>"
             ),
             plotlyOutput("variantFreqPlot", height = "auto")
           ),
           tabPanel(
             "This Week's Summary",
-            h6("May 27, 2022"),
+            h6("June 3, 2022"),
             p(
-              "The most recent data update includes samples taken May 17-23, 2022. During this sampling period:"
+              "The most recent data update includes samples taken May 24-30, 2022. During this sampling period:"
             ),
             tags$ul(
-              tags$li("The viral RNA load in Metro Plant influent is now trending lower, decreasing last week by 38% compared to the previous week"),
-              tags$li("The Omicron subvariant BA.2 made up 88% of the SARS-CoV-2 RNA in Metro Plant influent"),
+              tags$li("The viral RNA load in Metro Plant influent was essentially unchanged last week, increasing by only 2% compared to the previous week"),
+              tags$li("The Omicron subvariant BA.2 made up 76% of the SARS-CoV-2 RNA in Metro Plant influent, down from 88% the week earlier"),
               tags$li(
-                "BA.2 sub-lineage BA.2.12.1 made up 57% of the SARS-CoV-2 RNA in Metro Plant influent, up from 47% the previous week"
+                "BA.2 sub-lineage BA.2.12.1 made up 64% of the SARS-CoV-2 RNA in Metro Plant influent, up from 57% the previous week"
               ),
               tags$li(
-                "Omicron sub-variants BA.4 and BA.5 (not yet shown below) together made up 11% of the SARS-CoV-2 RNA in Metro Plant influent, up from 7% a week earlier"
+                "Omicron sub-variants BA.4 and BA.5 together made up 23% of the SARS-CoV-2 RNA in Metro Plant influent, up from 11% a week earlier"
+              ),
+              tags$li(
+                "BA.4 and BA.5 are expected to replace BA.2.12.1 as the dominant variants in Metro Plant wastewater in the coming weeks"
               )
             )
           )
@@ -128,7 +131,7 @@ fluidPage(
       ),
       tags$br(),
       "App last updated ",
-      "2022-05-27",
+      "2022-06-03",
       # using gh::gh() causes SAML error in production
       # gh::gh("GET /repos/Metropolitan-Council/covid-poops")[49][[1]] %>% as.Date(),
       style = "font-size: 1.5rem;
