07b1091694b5b891c2ce1fcc556e349a6274b244
lfletch0025 <laura.fletcher@metc.state.mn.us>
2023-04-21 09:11:26 -0500

    Adjusting x-axis label spacing

--- R/fig_plotly_variant_load.R
+++ R/fig_plotly_variant_load.R
@@ -180,7 +180,7 @@ base_ggplot <-
   ) +
   scale_x_date(
     name = "Date",
-    breaks = "2 months",
+    breaks = "3 months",
     date_labels = "%b '%y"
   ) +
   geom_hline(yintercept = 0, color = "gray30", size = 0.5) +
